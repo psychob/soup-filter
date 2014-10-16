216269a4d6b7a3ed8b6a9f160c2aad01ee858894
Andrzej Budzanowski <psychob.pl@gmail.com>
2014-10-16 14:02:33 +0200

    Added legacy versions

---
+++ legacy/soupio_filter-2.0.user.js
@@ -0,0 +1,76 @@
+﻿// ==UserScript==
+// @name           Soup.io filter
+// @namespace      psychobs-den
+// @description    This script show in bottom left corner few icons that help you filter soup
+// @include        http://*.soup.io/*
+// @exclude        http://www.soup.io/*
+// ==/UserScript==
+
+/**
+ * This script is (c) by andrzej budzanowski 2011
+ * All rights reserved!
+ *
+ * This is 2.0 version
+ *
+ * @link http://psychobpl.wordpress.com/
+ * @link http://psychob.soup.io/
+ */
+(function (_window) {
+ if (_window.top != _window.self)
+  return ;
+ _window.addEventListener("DOMContentLoaded", function () {
+   SOUP = {
+   'text' : '/images/icon_regular.png',
+   'link' : '/images/icon_link.png',
+   'quote' : '/images/icon_quote.png',
+   'image' : '/images/icon_image.png',
+   'video' : '/images/icon_video.png',
+   'file' : '/images/icon_file_audio.png',
+   'review' : '/images/icon_review.png',
+   'event' : '/images/icon_event.png'
+  };
+  //budujemy div-a
+  var d = document.createElement('div');
+  d.setAttribute('id', 'psychobs-den-filter');
+  //style dla kontenera
+  d.style.position = 'fixed';
+  d.style.bottom = '10px';
+  d.style.right = '10px';
+  d.style.zIndex = '100';
+  var add = '';
+  if (document.location.href.indexOf('mode=friends') != -1)
+   add = '&amp;mode=firends';
+  else add = '&amp;mode=own';
+  //sprawdzamy czy cos wyszukiwalismy
+  if (document.location.href.indexOf('search=') != -1)
+   {
+    var t = document.location.href.indexOf('search=') + 7;
+    var ta = '';
+    //sprawdzamy czy nie ma tam jeszcze & po naszym szukaniu
+    if (document.location.href.indexOf('&', t) != -1)
+     {
+      //jest
+      ta = document.location.href.substr(t, document.location.href.indexOf('&', t) - t);
+     } else
+     {
+      //nie ma
+      ta = document.location.href.substr(t);
+     }
+    add = add + '&amp;search=' + ta;
+   }
+  for (var prop in SOUP)
+   {
+    //tworzymy nowy obiekt
+    var tmp = document.createElement('div');
+    tmp.innerHTML = //jako ze nie chce tutaj tworzyc niewiadomo czego..
+    '<a href="?type=' + prop + add + '" title="Filter by: ' + prop + '"><img border="0" src="' +
+	SOUP[prop] + '" alt="' + prop +'" /></a>';
+    tmp.style.backgroundColor = '#000';
+    tmp.style.marginBottom = '2px';
+    if (typeof(tmp.style.borderRadius) != 'undefined')
+     tmp.style.borderRadius = '10px';
+    d.appendChild(tmp);
+   }
+  document.body.appendChild(d);
+ }, true);
+})(unsafeWindow);
---
+++ legacy/soupio_filter-3.1.user.js
@@ -0,0 +1,167 @@
+// ==UserScript==
+// @id soupio-filter@psychobpl.wordpress.com
+// @name Soup.io Filter Feature
+// @namespace http://psychobpl.wordpress.com/
+// @author Andrzej "PsychoB" Budzanowski <psychob.pl@gmail.com> http://psychobpl.wordpress.com/
+// @version 3.1
+// @description Add filter feature to soup.io souplogs.
+// @website http://psychobpl.wordpress.com/
+// @include *
+// @exclude http://www.soup.io/*
+// @exclude https://www.soup.io/*
+// @exclude http://status.soup.io/*
+// @run-at document-start
+// @iconURL https://dl.dropbox.com/u/35418266/dev/soupio/logo.png
+// ==/UserScript==
+
+//
+// soup.io filter ver 3.1
+// (c) 2011 - 2012 Andrzej Budznowski
+//
+//  * Version: 3.1 (20/06/2012)
+//    * Changed method for checking if we are on soup or if we not on soup
+//    * Exclude from execution in http://status.soup.io/
+//  * Version: 3.0 (7/03/2012)
+//    * Rewrite it (again...)
+//    * Work on Google Chrome
+//    * Added icon
+//    * Now work in all soup around the world!
+//    * Support filtering your and friends stream
+//  * Version: 2.0 (7/07/2011) 
+//    * Work with friend and own streams. 
+//    * Support since 
+//    * Support search   
+//  * Version: 1.1 (26/03/2011)  
+//  * Version: 1.0 (3/01/2011)  
+//
+
+var _sif = {
+ soup_images : {
+  'text' : '/images/icon_regular.png',
+  'link' : '/images/icon_link.png',
+  'quote' : '/images/icon_quote.png',
+  'image' : '/images/icon_image.png',
+  'video' : '/images/icon_video.png',
+  'file' : '/images/icon_file_audio.png',
+  'review' : '/images/icon_review.png',
+  'event' : '/images/icon_event.png'
+ },
+ _canYouRunIt : true,
+ init : function ()
+ {
+  if (document !== "undefined" && document.readyState == "complete") /// @todo 
+   _sif.pre_req();
+  else
+   window.addEventListener("DOMContentLoaded", _sif.pre_req, false);
+ },
+ pre_req : function ()
+ {
+  if (!_sif._canYouRunIt)
+   return ;
+  console.log('Check if SOUP.IO');
+   
+  // najpierw sprawdźmy czy jesteśmy w domenie soup.io
+  if (/\.soup\.io/.test(document.location.host))
+  {
+   _sif._canYouRunIt = true;
+   console.log("SOUP.IO status: true");
+  } else
+  {
+   // jeśli jednak nasz soup nie jest w domenie soup.io/
+   // sprawdzamy jeszcze czy istnieje zmienna SOUP
+   if (typeof unsafeWindow.SOUP == "undefined")
+   {
+    _sif._canYouRunIt = false;
+    console.log("SOUP.IO status: false");
+   } else
+   {
+    _sif._canYouRunIt = true;
+    console.log("SOUP.IO status: true");
+   }
+      
+  }
+    
+  _sif.show_blob();
+ },
+ show_blob : function ()
+ {
+  if (!_sif._canYouRunIt)
+   return ;
+	
+  // dodajemy linki
+  var ele = document.createElement("div");
+  ele.setAttribute("id", "__soupio_filter_cont");
+  ele.style.position = 'fixed';
+  ele.style.bottom = '2px';
+  ele.style.right = '2px';
+  ele.style.zIndex = '1999';
+   
+  _sif.addElement('text', ele);
+  _sif.addElement('link', ele);
+  _sif.addElement('quote', ele);
+  _sif.addElement('image', ele);
+  _sif.addElement('video', ele);
+  _sif.addElement('file', ele);
+  _sif.addElement('review', ele);
+  _sif.addElement('event', ele);
+   
+  document.body.appendChild(ele);
+ },
+ addElement : function (elementId, tocont)
+ {
+  var _l = document.createElement("div");
+  _l.style.display = "inline-block";
+  _l.style.backgroundColor = '#000';
+  if (!_l.style.borderRadius)
+   _l.style.borderRadius = '10px';
+  _l.style.margin = '2px';
+  //tworzymy a i img'a
+  var a_link = document.createElement("a");
+  a_link.style.display = 'inline-block';
+  var img = document.createElement("img");
+  img.setAttribute("src", _sif.soup_images[elementId]);
+  img.setAttribute("alt", elementId);
+  img.style.width = '32px';
+  img.style.height = '32px';
+  a_link.appendChild(img);
+  a_link.setAttribute('href', _sif.generate_link(elementId));
+  _l.appendChild(a_link);
+  tocont.appendChild(_l);
+ },
+ generate_link : function (id)
+ {
+  var hrf = document.location;
+  if (hrf.pathname == "/" &&
+   hrf.search == "")
+   {
+   return "/?type=" + id;
+  } else
+  {
+   //jeśli nie to sprawdzamy sobie spokojnie w czym jesteśmy...
+   //najpierw since, i friends
+   if (/mode=own/.test(hrf.search))
+   {
+    //jeśli jesteśmy w naszej przeszłości, to nic nie robimy
+    return hrf.pathname + "?type=" + id + "&mode=own";
+   } else if (/mode=friends/.test(hrf.search))
+   {
+    return hrf.pathname + "?type=" + id + "&mode=friends";
+   } else
+   {
+    //dwie możliwości, albo mamy search, albo nie wiemy nic
+    //pierwsza
+    var r = /search=([^&]+)/;
+    var match = r.exec(hrf.search);
+    if (match != null)
+    {
+     return hrf.pathname + "?type=" + id + "&search=" + match[1];
+    } else
+    {
+     return hrf.pathname + "?type=" + id; //nie wiem co sie dzieje
+    }
+   }
+  }
+ }
+};
+
+_sif.init();
